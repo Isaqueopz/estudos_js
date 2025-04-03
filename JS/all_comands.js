8cdc9cef96f99452f0f1a89e551a6b2ebb0306a0
Isaque Misael <sergioisaquepb@gmail.com>
2025-04-02 22:28:36 -0300

    studys of day

---
+++ JS/all_comands.js
@@ -0,0 +1,4 @@
+//NaN -> Not a number
+//parseInt('5') faz a conversão de uma string para um int
+//parseFloat('5') faz a conversão de uma string para um int
+//Number('5') faz a conversão de uma string para um int ou float
---
+++ JS/aula11_index.js
@@ -0,0 +1,3 @@
+alert('Com a nossa mensagem.')
+confirm('Deseja realmente apagar?'); // você captura um TRUE or FALSE
+prompt('Digite o seu nome:'); // inputs de caixas
