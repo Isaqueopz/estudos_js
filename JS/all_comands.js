f57fc662b14cfdb5934f14b47ca19cfe0abf8bf3
Isaque Misael <sergioisaquepb@gmail.com>
2025-04-07 11:29:07 -0300

    att

--- JS/all_comands.js
+++ JS/all_comands.js
@@ -53,4 +53,4 @@
     // COMPARAÇÃO
         // > >= < <= == === (igualdade estrita, valor e tipo) != () !== (diferente estrito, valor e tipo)
     // LÓGICOS
-        // && || !(não)
+        // && || !()
