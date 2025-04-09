537de50390dc7e6ccabade81a5c6627c08e38353
Isaque Misael <sergioisaquepb@gmail.com>
2025-04-09 08:21:42 -0300

    att2

---
+++ aulas_js/aula5_index.js
@@ -0,0 +1,4 @@
+// ola mundo
+console.log('Olá mundo!');
+console.log('Esse trecho será exibido no console');
+alert("Olá Mundo");
---
+++ aulas_js/aula6_index.js
@@ -0,0 +1,8 @@
+// LET -> VARIAVEL
+//Não pode conter espaços ou traços
+// usar camelCase
+
+let nome = 'João';
+// var nome = 'João';, também tem a mesma lógica
+
+console.log(nome, "nasceu em 1984.");
